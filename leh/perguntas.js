7d26448a36112129084804e00ba0975f05767279
leticiarit <159541328+leticiarit@users.noreply.github.com>
2024-09-23 08:47:35 -0300

    aula 567

---
+++ leh/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    '1',
+    'qual é o nome da música que a Luisa dedicou ao Chico Moedas?',
+    'O nome da música é Chico'
+)
+
+criaCartao(
+    '2',
+    'quantas músicas tem o útimo album dela?',
+    'Tem 24 músicas'
+)
+
+criaCartao(
+    '3',
+    'qual foi o depoimento da Luisa Sonza sobre a traição do Chico Moedas ?',
+    'Luisa fala "que se sentiu invalida, idiota, palhaça e trouxa)'
+)
